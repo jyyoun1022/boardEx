f193af1ad20e766304c6b7801b6dca25c35ed197
jyyoun1022 <jyyoun1022@naver.com>
2022-03-09 17:23:47 +0900

    검색처리 및 페이징

--- src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -3,11 +3,13 @@ package org.zerock.board.repository.search;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
@@ -19,6 +21,7 @@ import org.zerock.board.entity.QReply;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Log4j2
@@ -135,10 +138,22 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
             String prop = order.getProperty();
 
             PathBuilder orderByExpression = new PathBuilder(Board.class,"board");
-
-
+            tuple.orderBy(new OrderSpecifier(direction,orderByExpression.get(prop)));
         });
 
-        return null;
+        //page처리
+        tuple.offset(pageable.getOffset());
+        tuple.limit(pageable.getPageSize());
+
+        List<Tuple> result = tuple.fetch();
+
+        log.info(result);
+
+        long count = tuple.fetchCount();
+
+        log.info("COUNT : "+count);
+
+        return new PageImpl<Object[]>(
+                result.stream().map(t ->t.toArray()).collect(Collectors.toList()),pageable,count);
     }
 }
