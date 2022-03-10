a3c6c99ecc00a9c72fd92e290585c2025b174af2
jyyoun1022 <jyyoun1022@naver.com>
2022-03-10 20:26:53 +0900

    페이징처리

--- src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -3,13 +3,18 @@ package org.zerock.board.repository.search;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.JPQLQuery;
 import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.QBoard;
@@ -93,14 +98,33 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
                 booleanBuilder.and(conditionBuilder);
             }
 
-        List<Tuple> result = tuple.where(booleanBuilder)
-                .groupBy(board)
-                .fetch();
+         tuple.where(booleanBuilder);
+
+        Sort sort = pageable.getSort();
+
+        sort.stream().forEach(order -> {
+            Order direction = order.isAscending() ? Order.ASC : Order.DESC;
+            String property = order.getProperty();
+
+            PathBuilder orderByExpression = new PathBuilder(Board.class,"board");
+
+            tuple.orderBy(new OrderSpecifier(direction,orderByExpression.get(property)));
+        });
+
+        List<Tuple> result = tuple.groupBy(board).offset(pageable.getOffset()).limit(pageable.getPageSize()).fetch();
 
         log.info(result);
 
+        long count = tuple.fetchCount();
 
-        return null;
+        log.info("Count: "+count);
+
+
+        return new PageImpl<Object[]>(
+                result.stream().map(t ->t.toArray()).collect(Collectors.toList()),
+                pageable,
+                count
+        );
 
         }
 
