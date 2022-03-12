14ef2190ed68c4d62e38a92fc22e88b9a710f1ac
jyyoun1022 <jyyoun1022@naver.com>
2022-03-13 02:41:32 +0900

    댓글등록

--- src/main/java/org/zerock/board/controller/ReplyController.java
+++ src/main/java/org/zerock/board/controller/ReplyController.java
@@ -7,10 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.board.dto.ReplyDTO;
 import org.zerock.board.service.ReplyService;
 
@@ -31,6 +28,15 @@ public class ReplyController {
 
         return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
 
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO){
+
+        log.info(replyDTO);
+
+        Long rno = replyService.register(replyDTO);
+        return new ResponseEntity<>(rno,HttpStatus.OK);
     }
 
 }
